43f6f617dbc5754f47719cb6692e1dc1abab1a7f
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-11-15 21:37:56 +0100

    first commit

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PizzaController extends Controller
+{
+    public function index() {
+        $pizza = [
+            ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+            ['type' => 'capriciossa', 'base' => 'garlic crust'],
+            ['type' => 'veg supreme', 'base' => 'thin & crispy']
+        ];
+        return view('pizzas', [
+            'pizzas' => $pizza
+        ]);
+    }
+
+    public function show($id) {
+        return view('details', ['id'=> $id]);
+    }
+}
