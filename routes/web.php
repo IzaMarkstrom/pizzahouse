5f48debc6275fea3c8c1f5e8f02e6afa97a0fec3
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-11-16 16:57:10 +0100

    Small fixes

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pizzas', 'App\Http\Controllers\PizzaController@index');
+Route::get('/pizzas', 'App\Http\Controllers\PizzaController@index')->middleware('auth');
 Route::get('/pizzas/create', 'App\Http\Controllers\PizzaController@create');
 Route::post('/pizzas', 'App\Http\Controllers\PizzaController@store');
 // ID will otherwise show /pizzas/create as the ID and not the create page
-Route::get('/pizzas/{id}', 'App\Http\Controllers\PizzaController@show');
-Route::delete('/pizzas/{id}', 'App\Http\Controllers\PizzaController@destroy');
+Route::get('/pizzas/{id}', 'App\Http\Controllers\PizzaController@show')->middleware('auth');
+Route::delete('/pizzas/{id}', 'App\Http\Controllers\PizzaController@destroy')->middleware('auth');
 
 Auth::routes();
 
